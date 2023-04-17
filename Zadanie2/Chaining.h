2d8f0bb22b42192a588010443949aac13b36d9a6
Daniel Andrejčák <89748946+daniel-andrejcak@users.noreply.github.com>
2023-04-17 23:00:22 +0200

    kokot take jednoduche to bolo dopice

--- Zadanie2/Chaining.h
+++ Zadanie2/Chaining.h
@@ -1,7 +1,7 @@
 #include "bdd.h"
 
 void CHT_Init();
-void CHT_Insert(std::vector<std::string> expresion, NODE *node);
-NODE *CHT_Search(std::vector<std::string> expresion);
+void CHT_Insert(std::string expresion, NODE *node);
+NODE *CHT_Search(std::string expresion);
 void CHT_FreeMem();
 void visualize();
